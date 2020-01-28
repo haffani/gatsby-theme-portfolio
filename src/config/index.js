566f1cd2f28180fe00ea28644c3c97550fa1c139
Affani <elaffani95@gmail/com>
2020-01-28 23:44:20 +0100

    fix animation issues & mail address

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,12 @@ module.exports = {
   siteDescription:
     "Hamza AFFANI is a software engineer based in Casablanca, who specializes in developing high-quality payment systems and applications.",
   siteKeywords:
-    "Hamza AFFANI, Hamza, Affani, haffani, software engineer, front-end engineer, Java developer, javascript",
-  siteUrl: "https://hamza-affani.netlify.com",
+    "Hamza AFFANI, Hamza, Affani, haffani, software engineer, front-end engineer, Java developer, javascript, elaffani",
+  siteUrl: "https://haffani.netlify.com",
   siteLanguage: "en_US",
   name: "AFFANI Hamza",
   location: "Casablanca, Morocco",
-  email: "elaffani95@@gmail.com",
+  email: "elaffani95@gmail.com",
   github: "https://github.com/liffanu",
   twitterHandle: "@elaffani95",
   socialMedia: [
@@ -54,19 +54,19 @@ module.exports = {
   navyColor: "#0a192f",
   darkNavyColor: "#020c1b",
 
-  srConfig: (delay = 200) => ({
+  srConfig: (delay = 150) => ({
     origin: "bottom",
     distance: "20px",
     duration: 500,
     delay,
-    rotate: { x: 0, y: 5, z: 10 },
+    rotate: { x: 0, y: 0, z: 0 },
     opacity: 0,
     scale: 1,
     easing: "cubic-bezier(0.645, 0.045, 0.355, 1)",
     mobile: true,
     reset: false,
     useDelay: "always",
-    viewFactor: 0.3,
+    viewFactor: 0.25,
     viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
   }),
 }
